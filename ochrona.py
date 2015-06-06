3ee58c70635eed4131f6ed7f5d8c36fb74d03acd
Podolecp <piotrpodolec@gamil.com>
2015-06-06 17:10:29 +0200

    routing chyba smiga

--- ochrona.py
+++ ochrona.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request
 
 app = Flask(__name__)
 message = ''
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('login.html', message = message)
+    return render_template('login.html', message='')
 @app.route('/main')
 def display_main():
     return u'Shity i notatki'
 
 @app.route('/login', methods=['GET', 'POST'])
-def veryfication(login, password):
+def veryfication():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        print 'w postcie'
     if login == 'Karol' and password == 'piczka':
-        render_template("main.html")
-    message = 'Uncorrect login or password'
-    index()
+        print 'karol piczka'
+        return render_template("main.html")
+    print 'niepoprawne dane'
+    return render_template('login.html', messege='Niepoprawne dane')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
