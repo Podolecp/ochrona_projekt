ade7c49fd5a3193c95ff7658539026a5a88f3bc7
Podolecp <piotrpodolec@gamil.com>
2015-06-06 15:49:15 +0200

    prace trwają

--- ochrona.py
+++ ochrona.py
@@ -2,15 +2,21 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+message = ''
 
-
-@app.route('/')
-def hello_world():
-    return render_template('login.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('login.html', message = message)
 @app.route('/main')
 def display_main():
     return u'Shity i notatki'
 
-
+@app.route('/login', methods=['GET', 'POST'])
+def veryfication(login, password):
+    if login == 'Karol' and password == 'piczka':
+        render_template("main.html")
+    message = 'Uncorrect login or password'
+    index()
 if __name__ == '__main__':
+    app.debug = True
     app.run()
